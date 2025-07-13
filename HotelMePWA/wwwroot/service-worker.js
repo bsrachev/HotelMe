566b62aad60146568eba1a0be77a912d6055c4cf
bsrachev <b.s.rachev@gmail.com>
2025-07-13 20:45:39 +0300

    PWA functionality

--- HotelMePWA/wwwroot/service-worker.js
+++ HotelMePWA/wwwroot/service-worker.js
@@ -1,4 +1,29 @@
-// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+self.importScripts('service-worker-assets.js');
+
+const CACHE_NAME = 'hotelme-cache-v1';
+const ASSETS = self.assetsManifest.assets
+    .map(a => a.url)
+    .concat([
+        '/',
+        'index.html',
+        'manifest.json'
+    ]);
+
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+    evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', evt => {
+    if (evt.request.method !== 'GET') return;
+    evt.respondWith(
+        caches.match(evt.request)
+            .then(cacheRes => cacheRes || fetch(evt.request))
+    );
+});
